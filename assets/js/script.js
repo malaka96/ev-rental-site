437de18ecc7385ccf0845aa2dec419064f21c797
malaka96 <madhubhashana655@gmail.com>
2025-08-03 01:48:55 +0530

    almost finished home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.custom-navbar');
-    if (window.scrollY > 50) { /* Trigger after scrolling 50px */
-        navbar.classList.remove('bg-transparent');
-    } else {
-        navbar.classList.add('bg-transparent');
-    }
+// Navbar color transition on scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.custom-navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
+// window.addEventListener('scroll', function () {
+//     const navbar = document.querySelector('.custom-navbar');
+//     if (window.scrollY > 50) { /* Trigger after scrolling 50px */
+//         navbar.classList.remove('bg-transparent');
+//     } else {
+//         navbar.classList.add('bg-transparent');
+//     }
+// });
